936211ae140b435ac4e1238f0f7bc442aceefaa7
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-08-15 00:05:20 +0000

    atividade aula3

---
+++ aula_3.py
@@ -0,0 +1,57 @@
+# frutas = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#         print(fruit)
+# for x in range(10):
+#     print(x)
+    
+# num = 0
+# while num < 8:
+#     num += 1
+#     if num == 4:
+#         break    print(num)
+# else:
+#     print("sair do loop")
+
+
+# # print (frutas[:0)
+# # frutas+=temperos
+# # frutas.remove ("banana")    
+# # del frutas [1]
+# # print(frutas.clear())
+# # print (fruta[8])
+# sorted(frutas)
+# print(frutas)
+
+# 
+# lista1 = [1, 2, 3]
+# lista2 = lista1
+# lista3 = [1, 2, 3]
+
+# print(f"ID da lista1: {id(lista1)}")
+# print(f"ID da lista2: {id(lista2)}")
+# print(f"ID da lista3: {id(lista3)}")
+
+# if id(lista1) == id(lista2):
+#     print("lista1 e lista2 apontam para o mesmo objeto")
+# else:
+#     print("lista1 e lista2 não apontam para o mesmo objeto")
+
+# if id(lista1) == id(lista3):
+#     print("lista1 e lista3 apontam para o mesmo objeto")
+# else:
+#     print("lista1 e lista3 não apontam para o mesmo objeto")
+
+# for fruta in frutas:
+#     print(fruta)
+# print(frutas.count("laranja"))
+
+lista = ["casa","caderno"]
+ista2 = ["gato","corda")]
+lista += lista2
+print(lista)
+lista.remove ("casa")
+
+
+
+
+
