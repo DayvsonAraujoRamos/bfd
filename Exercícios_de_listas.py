886fb508cdbadb1c08970b854c4cb1bb8020fb7a
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-08-25 02:54:02 +0000

    imcompleto

---
+++ Exercícios_de_listas.py
@@ -0,0 +1,41 @@
+# livros = ["O principe","A batalha do apocalipse",'A arte da guerra' ]
+# print("Lista dos livros:", livros)
+
+# print("O primeiro livro é:", livros[0])
+# print("O último livro é:", livros[-1])
+
+# livros.append("Vidas secas")
+# livros.append("Dom Casmurro")
+# print(livros)
+
+# livros.insert(1,"Duna")
+# print(livros)
+
+# if "Silêncio  dos inocentes" in livros:
+#       livros.remove("Silêncio dos  inocentes")
+# else: 
+#       print("Livro não encontrado")
+
+# # num = [1, 2, 3, 2, 4, 2, 5]
+# # print("O número 2 aparece",num.count(2), "vezes")
+
+# for livro in livros:
+#       print(f"O livro {livro} é interessante")
+
+# idades = [12, 18, 25, 14, 30]
+# for idade in idades:
+#     if idade >= 18:
+#         print(idade)
+
+# valores = [10, 20, 30, 40]
+# soma = 0
+# for x in valores:
+#     soma += x
+#     print("A soma dos valores é",soma)
+
+
+
+
+
+
+
