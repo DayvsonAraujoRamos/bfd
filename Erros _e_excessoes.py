0cc2677be350de623a521287eacfe24f6da5ff34
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-09-04 23:46:08 +0000

    falta terminar

---
+++ Erros _e_excessoes.py
@@ -0,0 +1,41 @@
+# def num():
+#     try:
+#         Numero = int(input("escreva um número "))
+#     except ValueError:
+#         print("O número informado deve ser inteiro")
+# num()
+# =====================================
+# try:
+#     num = int(input("digite um numero "))
+#     num2 = int(input("digite outro numero "))
+#     mult = num*num2
+#     print(mult)
+# except ValueError:
+#     print("digite um numero inteiro")
+# ===========================================
+# try:
+#     num = int(input("digite um numero "))
+# except ValueError:
+#     print("digite um numero inteiro")
+# else:
+#     print(num)
+# =========================
+# try:
+#     arquivo = open("dados.txt","r")
+#     print(arquivo.read())
+# except FileNotFoundError:
+#     print("arquivo nao encontrado")
+# finally:
+#     print("Encerrando Programa")
+# =================================
+def divide(a,b):
+    raise    
+try:
+    
+       
+      
+
+
+           
+ 
+
