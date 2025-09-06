c9d6862914645397e4222b4b8370a450ca44959d
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-09-06 07:28:38 +0000

    falta uma questao

--- Erros _e_excessoes.py
+++ Erros _e_excessoes.py
@@ -1,15 +1,15 @@
 # def num():
 #     try:
-#         Numero = int(input("escreva um número "))
+#         numero = int(input("digite um número "))
+#         print(f"voce digitou: {numero}")
 #     except ValueError:
 #         print("O número informado deve ser inteiro")
 # num()
 # =====================================
 # try:
-#     num = int(input("digite um numero "))
-#     num2 = int(input("digite outro numero "))
-#     mult = num*num2
-#     print(mult)
+#     n1 = int(input("digite um numero "))
+#     n2 = int(input("digite outro numero "))
+#     print(f"o valor da multiplicaçao é:,{n1 * n2}")
 # except ValueError:
 #     print("digite um numero inteiro")
 # ===========================================
@@ -18,7 +18,7 @@
 # except ValueError:
 #     print("digite um numero inteiro")
 # else:
-#     print(num)
+#     print("numero digitado com sucesso")
 # =========================
 # try:
 #     arquivo = open("dados.txt","r")
@@ -26,12 +26,42 @@
 # except FileNotFoundError:
 #     print("arquivo nao encontrado")
 # finally:
-#     print("Encerrando Programa")
+#     print("encerrando programa")
 # =================================
+# def dividir(a, b):
+#     if b == 0:
+#         raise ZeroDivisionError("divisao por zero nao permitida!")
+#     return a / b 
+# =======================================      
+# class IdadeInvalidaError(Exception):
+#     pass
+# def cadastrar_idade(idade):
+#     if idade < 0:
+#         raise IdadeInvalidaError("Idade não pode ser negativa!")
+#     print(f"Idade cadastrada: {idade}")
+#     =========================================
+# try:
+#     a = float(input("Digite o primeiro número: "))
+#     b = float(input("Digite o segundo número: "))
+#     print(f"o resultado é: {a / b}")
+# except ValueError:
+#     print("Digite apenas números válidos!")
+# except ZeroDivisionError:
+#     print( "Não é possível dividir por zero!")
+#     ===================================
+# try:
+#     n = int(input("digite um número inteiro: "))
+# except ValueError:
+#     print("valor inválido!")
+# else:
+#     if n % 2 == 0:
+#         print("o número digitado é par.")
+#     else:
+#         print("o número digitado é ímpar.")
+# finally:
+#     print("fim do programa")
+# ================================
 
-    
-       
-      
 
 
            
