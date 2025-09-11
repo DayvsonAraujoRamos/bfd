f93e54cd489c5231eac6d59e8559e4119ca7ce2f
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-09-11 23:11:20 +0000

    falta pouco

--- Exercicio POO classes e objetos.py
+++ Exercicio POO classes e objetos.py
@@ -30,10 +30,55 @@
 # carro1.ano = 2023
 # carro1.mostrar_informaçoes()
 # ========================================================
-class ContaBancaria:
-    def __int_(self,titular,saldo=0):
-        self.titular = titular
-        self.saldo = saldo
-        
-        def depositar(self,valor):
-            
+# class ContaBancaria:
+#     def __init__(self,titular,saldo=0):
+#         self.titular = titular
+#         self.saldo = saldo
+
+#     def depositar(self,valor):
+#         self.saldo += valor
+#         print(f"Foi feito um deposito de {valor} , seu saldo é {self.saldo}")
+
+#     def sacar(self,valor):
+#         if valor <= self.saldo:
+#             self.saldo -= valor
+#             print(f"Foi feito um saque de {valor} ,seu saldo é {self.saldo}")
+#             return True
+#         else:
+#             print("saldo insuficiente")
+#             return False
+           
+# conta2 = ContaBancaria("Pedro",1000)
+# conta3 = ContaBancaria("João,0")
+# if conta2.sacar(100):
+#     print("Saque realizado com sucesso")
+# else:
+#     print("Não foi possível efetuar saque")
+# ===============================================
+class Aluno:
+    def __init__(self,nome,nota):
+        self.nome = nome
+        self.nota = nota
+
+    def __str__(self):
+       return f"Aluno: {self.nome}, Nota: {self.nota}"
+class Turma:
+    def __init__(self):
+        self.alunos = []
+    def adicionar_aluno(self,aluno):
+       self.alunos.append(aluno)
+
+aluno1 = Aluno("Pedro",10)
+aluno2 = Aluno("Ana",9)
+aluno3 = Aluno("José",8.5)
+aluno4 = Aluno("Maria",9)
+
+turma = Turma()
+turma.adicionar_aluno(aluno1)
+turma.adicionar_aluno(aluno2)
+turma.adicionar_aluno(aluno3)
+turma.adicionar_aluno(aluno4)
+
+for aluno in turma.alunos:
+    print(aluno)
+
