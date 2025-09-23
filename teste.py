bf0ee4c9dc52a64fb59c9d20ca5a153d39b93dcc
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-09-23 22:36:11 +0000

    exercicio finalizado

--- teste.py
+++ teste.py
@@ -1,38 +1,39 @@
-# Classe base
-class Animal:
-    def __init__(self, nome, idade):
-        self._nome = nome       # protegido
-        self.__idade = idade    # privado
+# # Classe base
+# class Animal:
+#     def __init__(self, nome, idade):
+#         self._nome = nome       # protegido
+#         self.__idade = idade    # privado
 
-    # Encapsulamento com getters/setters
-    def get_idade(self):
-        return self.__idade
+#     # Encapsulamento com getters/setters
+#     def get_idade(self):
+#         return self.__idade
 
-    def set_idade(self, idade):
-        if idade >= 0:
-            self.__idade = idade
-        else:
-            print("Idade inválida!")
+#     def set_idade(self, idade):
+#         if idade >= 0:
+#             self.__idade = idade
+#         else:
+#             print("Idade inválida!")
 
-    def fazer_som(self):
-        print(f"{self._nome} faz um som.")
+#     def fazer_som(self):
+#         print(f"{self._nome} faz um som.")
 
-# Classe filha
-class Cachorro(Animal):
-    def __init__(self, nome, idade, raca):
-        super().__init__(nome, idade)  # herança
-        self.raca = raca
+# # Classe filha
+# class Cachorro(Animal):
+#     def __init__(self, nome, idade, raca):
+#         super().__init__(nome, idade)  # herança
+#         self.raca = raca
 
-    # Polimorfismo: sobrescrevendo método
-    def fazer_som(self):
-        print(f"{self._nome} está latindo!")
+#     # Polimorfismo: sobrescrevendo método
+#     def fazer_som(self):
+#         print(f"{self._nome} está latindo!")
 
-# Testando
-dog = Cachorro("Rex", 3, "Labrador")
-dog.fazer_som()                         # Polimorfismo
-print(f"Idade: {dog.get_idade()}")      # Encapsulamento
-dog.set_idade(5)
-print(f"Nova idade: {dog.get_idade()}") # Encapsulamento
+# # Testando
+# dog = Cachorro("Rex", 3, "Labrador")
+# dog.fazer_som()                         # Polimorfismo
+# print(f"Idade: {dog.get_idade()}")      # Encapsulamento
+# dog.set_idade(5)
+# print(f"Nova idade: {dog.get_idade()}") # Encapsulamento
+
 
 
 
