e373c27224ca39f5728447093098336b1f8bfecf
DayvsonAraujoRamos <121202123+DayvsonAraujoRamos@users.noreply.github.com>
2025-10-10 19:47:58 +0000

    Terminado

--- exercicio classes abstratas.py
+++ exercicio classes abstratas.py
@@ -1,43 +1,43 @@
-# from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 
-# class Animal(ABC):
-#     @abstractmethod
-#     def falar(self):  
-#      pass
-# class Cachorro(Animal):
-#     def falar(self):
-#        return "Auau"
-# class Gato(Animal):
-#     def falar(self):
-#        return "Miau"   
-# dog = Cachorro()   
-# cat = Gato()
-# print(dog.falar())
-# print(cat.falar())  
+class Animal(ABC):
+    @abstractmethod
+    def falar(self):  
+     pass
+class Cachorro(Animal):
+    def falar(self):
+       return "Auau"
+class Gato(Animal):
+    def falar(self):
+       return "Miau"   
+dog = Cachorro()   
+cat = Gato()
+print(dog.falar())
+print(cat.falar())  
 # ===================
-# rex = Animal()
+rex = Animal()
 # A classe não pode ser instanciada,só serve de modelo para as subclasses
 # =============================
-# from abc import ABC, abstractmethod
-# class FormaGeometrica(ABC):
-#     @abstractmethod
-#     def area():
-#        pass
-#     def perimetro():
-#        pass
-# class Retangulo(FormaGeometrica):
-#     def __init__(self,base,altura):
-#       self.base = base
-#       self.altura = altura
+from abc import ABC, abstractmethod
+class FormaGeometrica(ABC):
+    @abstractmethod
+    def area():
+       pass
+    def perimetro():
+       pass
+class Retangulo(FormaGeometrica):
+    def __init__(self,base,altura):
+      self.base = base
+      self.altura = altura
 
-#     def area(self):
-#        return self.base *self.altura
-#     def perimetro(self):
-#        return 2 * (self.base + self.altura)
+    def area(self):
+       return self.base *self.altura
+    def perimetro(self):
+       return 2 * (self.base + self.altura)
     
-# retangulo1 = Retangulo(6,7) 
-# print(f" A área do retangulo é {retangulo1.area()}")   
-# print(f" O perimetro do retangulo é {retangulo1.perimetro()}")
+retangulo1 = Retangulo(6,7) 
+print(f" A área do retangulo é {retangulo1.area()}")   
+print(f" O perimetro do retangulo é {retangulo1.perimetro()}")
 # ============================  
 from abc import ABC, abstractmethod
 
@@ -58,6 +58,6 @@ class Carro(Transporte):
     #     return "O carro está parando"
     
 fusca = Carro()
-# Ocorre um erro pois é obrigatóio também imcrementar o metodo parar()    
+# Ocorre um erro pois é obrigatório também incrementar o metodo parar()    
 print(fusca.mover())
 print(fusca.parar())
